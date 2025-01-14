f12b67eb4ead873e04e721eedba539d29be184ac
netflypsb <154873617+netflypsb@users.noreply.github.com>
2025-01-14 10:32:04 +0800

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Card } from "@/components/ui/card";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const Contact = () => {
   const router = useRouter();
